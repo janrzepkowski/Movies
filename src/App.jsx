9c32168695d45b223ea59f7f8d4884ae18138d09
janrzepkowski <jan.rzepkowski21@gmail.com>
2025-02-09 19:00:08 +0100

    Assets & styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import React from "react";
 
 const App = () => {
-  return <h1>Movies</h1>;
+  return (
+    <div className="text-center">
+      <h1>Movies</h1>
+    </div>
+  );
 };
 
 export default App;
