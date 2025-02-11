77926d4a960e7293effa268a116a904f34877fdf
janrzepkowski <jan.rzepkowski21@gmail.com>
2025-02-11 13:51:31 +0100

    Movie card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -59,7 +60,7 @@ const App = () => {
           <img src="/hero.png" alt="Hero Banner" />
           <h1>
             Track <span className="text-gradient">movies</span> you’ve watched.
-            Save those you want to see. Tell your friends what’s good.{" "}
+            Tell your friends what’s good.{" "}
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
@@ -73,9 +74,7 @@ const App = () => {
           ) : (
             <ul>
               {movies.map((movie) => (
-                <p key={movie.id} className="text-white">
-                  {movie.title}
-                </p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
