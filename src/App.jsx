48dfa03d9ebcc93d3fa7b6a02fdfec0092a79ea4
janrzepkowski <jan.rzepkowski21@gmail.com>
2025-02-11 13:59:21 +0100

    Search implementation

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ const App = () => {
   const [movies, setmovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setErrorMessage("");
     setIsLoading(true);
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -49,8 +51,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
